ba1db358bf160cf87ab089ebf2a461632455c38d
trierbo <trier@keessi.com>
2017-08-09 09:30:05 +0800

    master

---
+++ src/test/java/org/trier/wechat/Time.java
@@ -0,0 +1,13 @@
+package org.trier.wechat;
+
+import org.junit.Test;
+
+/**
+ * Created by trier on 2017/7/4.
+ */
+public class Time {
+    @Test
+    public void time(){
+        System.out.println(System.currentTimeMillis()/1000);
+    }
+}
